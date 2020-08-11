58e95baeaefa0e3bccd35c8163477ebc978413e5
Robert Claesson <rclaesson@itrsgroup.com>
2020-08-11 16:13:06 +0200

    changed text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hi There! Bye There! Still there? Yep, seems so.
+          Hi There! Bye There! Still there? Yep, seems so. See changes?
         </p>
       </div>
     );
